6c8db8892d9039f2126e1a91d8072071aed52623
Juliano Correa <juliano.correa@sou.unifeob.edu.br>
2024-09-25 20:24:10 -0300

    Envio
    Envio de arquivos.

---
+++ aulas/Aula016_OperadoresAritmeticos/src/curso/OperadoresAritmeticos.java
@@ -0,0 +1,27 @@
+package curso;
+
+import java.util.Scanner;
+
+public class OperadoresAritmeticos {
+	
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 2;
+		double z = 2.5;
+		Scanner sc = new Scanner(System.in);
+		/*
+		  Operadores
+		  +  -  *  /   %
+		 */
+		z = sc.nextDouble();
+		double resultado = a / z;
+		 System.out.println("Resultado: " + resultado);
+	}
+
+}
+
+
+
+
+
+
---
+++ aulas/Aula046/src/com/github/cidacastello/modelo/Carro.java
@@ -0,0 +1,26 @@
+package com.github.cidacastello.modelo;
+
+// imports
+//declaração da classe
+
+public class Carro {
+	
+	// variáves de instância (atributos, campos)
+	// modificador de acesso - tipo - nome da variável de instância
+	
+	public String marca;
+	public String modelo;
+	public int anoFrabricacao;
+	public String cor;
+	public double preco;
+	
+	
+	// métodos - construtores
+	
+
+}
+
+
+
+
+
